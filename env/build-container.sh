f9ac8c161d6590013befa8c2e7e7a28620edb05d
Andrew Blair <andrew.blair@ucsf.edu>
2023-05-22 12:41:00 -0700

    rm env

--- env/build-container.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash 
-# docker build -t docker.synapse.org/syn51118207/mprabase:amd64-v0.1.0 . # NOTE: For intel
-docker build -t docker.synapse.org/syn51118207/mprabase:arm64-v0.1.1 . # NOTE: For m1/m2 iOS
