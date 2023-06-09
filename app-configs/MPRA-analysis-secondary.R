0cc54c6e0a0bd0b9213cff059b3e8d559e853eeb
apblair <apblair95@gmail.com>
2023-06-09 16:14:51 -0700

    Update yaml build and secondary analysis

--- app-configs/MPRA-analysis-secondary.R
+++ app-configs/MPRA-analysis-secondary.R
@@ -1,12 +1,24 @@
 library(iSEE)
 
+# Summarized Experiment Requirement
+# dataset1: genomeAnnotation
+# dataset2: Genomeannotation
+# dataset3: genome_annotation <- ENCODE/GENCODE
+
 initial <- list()
 
 initial[["RowDataTable1"]]  <- RowDataTable()
 
+# reference: https://isee.github.io/iSEE/reference/SampleAssayPlot-class.html
 initial[["SampleAssayPlot1"]]  <- SampleAssayPlot(YAxisSampleName='rep1',
                 XAxis='Sample name',
                 XAxisSampleName='rep1',
                 Assay='ActivityScore',
                 ColorByDefaultColor = 'red' ,
                 ColorBySampleNameAssay = 'ActivityScore')
+
+
+# initial[["ComplexHeatmapPlot1"]] <- ComplexHeatmapPlot(Assay="ActivityScore")
+
+
+# initial[["GenomeAnnotationPieChart"]] <- PieChart(genomic_annotation, summarized_experiment, enhancers, pro) 
